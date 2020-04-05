9c115f687e6061559107e4568196740dfbee4f8c
mdb513 <mdb513@mdb513s-MBP.attlocal.net>
2020-04-05 00:48:50 -0400

    Created template code for a post endpoint that returns a json response

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+type travel struct {
+	Lat    float64 `json:"lat"`
+	Lon    float64 `json:"lon"`
+	Radius int     `json:"radius"`
+}
+
+func handleLoginRequest(w http.ResponseWriter, r *http.Request) {
+	response := travel{Lat: 39.1702, Lon: -76.8538, Radius: 200}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/loginRequest", handleLoginRequest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
