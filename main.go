b368ae8c06af15b369d55aa84d0e306c86604e1f
mdb513 <mdb513@mdb513s-MBP.attlocal.net>
2020-04-06 13:48:27 -0400

    Mapping request body to type - created struct type for request - created struct type for response

--- main.go
+++ main.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type travel struct {
+type currentGeo struct {
 	Lat    float64 `json:"lat"`
 	Lon    float64 `json:"lon"`
 	Radius int     `json:"radius"`
 }
 
+type travelResponse struct {
+	Current currentGeo `json:"currentGeo"`
+}
+
+type loginRequest struct {
+	Username      string `json:"username"`
+	UnixTimestamp int    `json:"unix_timestamp"`
+	EventUUID     string `json:"event_uuid"`
+	IPAddress     string `json:"ip_address"`
+}
+
 func handleLoginRequest(w http.ResponseWriter, r *http.Request) {
-	response := travel{Lat: 39.1702, Lon: -76.8538, Radius: 200}
+	var request loginRequest
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048567)) // Limiting the size of the request body
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &request); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(400)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	response := travelResponse{currentGeo{Lat: 39.1702, Lon: -76.8538, Radius: 200}}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
